beb819f86bb0cb714ae1f202c241310f4d5c3c5b
Dhanashree <dhanashreegosavi183@gmail.com>
2025-02-10 16:47:12 +0530

    added video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -2,45 +2,45 @@ import mongoose from " mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
-    {
-        videoFile: {
-            type: String, // cloudinary url
-            required: true,
-        },
-        thumbnail: {
-            type: String, // cloudinary url
-            required: true,
-        },
-        title: {
-            type: String, 
-            required: true,
-        },
-        description: {
-            type: String, 
-            required: true,
-        },
-        duration: {
-            type: Number, // not given by user --> we get it from cloudinary url (cloudinary sends description of video - url, its duration)
-            required: true,
-        },
-        views: {
-            type: Number,
-            default: 0,
-        },
-        isPublished: {
-            type: Boolean,
-            default: true,
-        },
-        owner:{
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        }
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
     },
-    {
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // not given by user --> we get it from cloudinary url (cloudinary sends description of video - url, its duration)
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
     timestamps: true,
-    }
-)
+  }
+);
 
-videoSchema.plugin(mongooseAggregatePaginate) // add our own plugins
+videoSchema.plugin(mongooseAggregatePaginate); // add our own plugins
 
 export const Video = mongoose.model("Video", videoSchema);
